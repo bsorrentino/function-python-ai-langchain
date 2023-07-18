3789087f862ef00883c95b163e6c1aa6daa238e8
bsorrentino <bartolomeo.sorrentino@gmail.com>
2023-07-18 14:35:34 +0000

    fix:(function_app.py): add api version

--- function_app.py
+++ function_app.py
@@ -30,6 +30,8 @@ def main(req):
     AZURE_OPENAI_SERVICE = os.environ.get("AZURE_OPENAI_SERVICE") or "myopenai"
     AZURE_OPENAI_GPT_DEPLOYMENT = os.environ.get("AZURE_OPENAI_GPT_DEPLOYMENT") or "davinci"
     AZURE_OPENAI_CHATGPT_DEPLOYMENT = os.environ.get("AZURE_OPENAI_CHATGPT_DEPLOYMENT") or "chat" #GPT turbo
+    AZURE_OPENAI_API_VERSION = os.environ.get("AZURE_OPENAI_API_VERSION") or "2023-06-01-preview" # this may change in the future
+
     if 'AZURE_OPENAI_KEY' not in os.environ:
         raise RuntimeError("No 'AZURE_OPENAI_KEY' env var set.  Please see Readme.")
 
@@ -37,13 +39,13 @@ def main(req):
     openai.api_key = AZURE_OPENAI_KEY
     openai.api_base = AZURE_OPENAI_ENDPOINT # your endpoint should look like the following https://YOUR_RESOURCE_NAME.openai.azure.com/
     openai.api_type = 'azure'
-    openai.api_version = '2023-05-15' # this may change in the future
-                                      # for langchain, set this version in environment variables using OPENAI_API_VERSION
+    openai.api_version = AZURE_OPENAI_API_VERSION
+                                     
 
     if bool(USE_LANGCHAIN):
         logging.info('Using Langchain')
 
-        llm = AzureOpenAI(deployment_name=AZURE_OPENAI_CHATGPT_DEPLOYMENT, temperature=0.3, openai_api_key=AZURE_OPENAI_KEY)
+        llm = AzureOpenAI(deployment_name=AZURE_OPENAI_CHATGPT_DEPLOYMENT, temperature=0.3, openai_api_key=AZURE_OPENAI_KEY, openai_api_version=AZURE_OPENAI_API_VERSION)
         llm_prompt = PromptTemplate(
             input_variables=["human_prompt"],
             template="The following is a conversation with an AI assistant. The assistant is helpful.\n\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman: {human_prompt}?",
